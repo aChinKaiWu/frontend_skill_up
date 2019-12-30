d055ca45618040a66315cb0bf22e66cf3db8d5cf
aChloeShen <chloe.shen@access-company.com>
2019-12-30 17:52:27 +0800

    [W1D1][ADD] refresh button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,37 @@
 import React from 'react'
-import ScenarioContainer from './containers/ScenarioContainer/ScenarioContainer'
+import { Button, Icon } from '@material-ui/core'
+import { makeStyles } from '@material-ui/styles'
+
+const useStyles = makeStyles({
+  button: {
+    background: '#ffffff',
+    border: 'solid 2px #525B5C',
+    borderRadius: 16,
+    color: '#525B5C',
+    font: 'Bold 14px YuGothic',
+    height: 32,
+    left: 25,
+    lineHeight: 1,
+    opacity: 1,
+    padding: 0,
+    top: 79,
+    width: 90,
+  },
+  icon: {
+    color: '#525B5C',
+    transform: 'rotateY(180deg)',
+  },
+})
 
 const App: React.FC = () => {
+  const classes = useStyles()
+
   return (
     <div>
-      <ScenarioContainer />
+      <Button className={classes.button} variant="outlined">
+        <Icon className={classes.icon}>refresh</Icon>
+        更新
+      </Button>
     </div>
   )
 }
