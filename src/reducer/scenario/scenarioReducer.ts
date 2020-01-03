54bfa9a1fc372efa7ffb49858e2ad94c7ebb264e
Chin-Kai Wu <chin-kai.wu@access-company.com>
2020-01-03 23:30:27 +0800

    [ADD] scenario action into reducer

--- src/reducer/scenario/scenarioReducer.ts
+++ src/reducer/scenario/scenarioReducer.ts
@@ -1,4 +1,5 @@
 import { AnyAction } from 'redux'
+import { scenarioActionTypes } from './scenarioAction'
 import { ScenarioList } from '../../model/scenario'
 
 export interface ScenarioState {
@@ -11,6 +12,11 @@ export const scenarioInitState: ScenarioState = {
 
 export default function scenarioReducer(state: ScenarioState = scenarioInitState, action: AnyAction) {
   switch (action.type) {
+    case scenarioActionTypes.GET_SCENARIO_LIST_SUCCESS:
+      return {
+        ...state,
+        scenarioList: action.payload,
+      }
     default:
       return state
   }
