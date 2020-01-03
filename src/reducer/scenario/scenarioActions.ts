97dc7b0e6791f83b2d8b12c9add8fcd24cc64653
aChiweiHuang <chiwei.huang@access-company.com>
2020-01-03 16:49:09 +0800

    implement delete scenario actions

--- src/reducer/scenario/scenarioActions.ts
+++ src/reducer/scenario/scenarioActions.ts
@@ -3,6 +3,9 @@ import { Scenario } from '../../model/scenario'
 export const scenarioActionTypes = {
   GET_SCENARIO_LIST: '@scenarios/GET_LIST',
   GET_SCENARIO_LIST_SUCCESS: '@scenarios/GET_LIST_SUCCESS',
+
+  DELETE_SCENARIO_LIST: '@scenarios/DELETE_LIST',
+  DELETE_SCENARIO_LIST_SUCCESS: '@scenarios/DELETE_LIST_SUCCESS'
 }
 
 export const getScenarioListAction = () => ({
@@ -11,5 +14,10 @@ export const getScenarioListAction = () => ({
 
 export const getScenarioListSuccessAction = (payload: Scenario[]) => ({
   type: scenarioActionTypes.GET_SCENARIO_LIST_SUCCESS,
+  payload,
+})
+
+export const deleteScenarioList = (payload: number[]) => ({
+  type: scenarioActionTypes.DELETE_SCENARIO_LIST,
   payload,
 })
