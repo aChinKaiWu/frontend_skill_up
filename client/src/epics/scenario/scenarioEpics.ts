85125ec11c884277fd2cf44a870db1162dee518f
yoyopan <yoyo.pan@access-company.com>
2020-01-06 18:56:32 +0800

    Real get & delete api integration

--- client/src/epics/scenario/scenarioEpics.ts
+++ client/src/epics/scenario/scenarioEpics.ts
@@ -5,19 +5,18 @@ import {
   getScenarioListFailedAction,
   deleteScenariosSuccessAction,
   deleteScenariosFailedActionAction,
-  ScenarioList,
 } from '../../reducer/scenario/scenarioActions'
 import { ActionsObservable, ofType } from 'redux-observable'
 import { of } from 'rxjs'
 import { catchError, exhaustMap, map, switchMap } from 'rxjs/operators'
-import mockData from './mockData'
+import { ajax, AjaxResponse } from 'rxjs/ajax'
 
 export const getScenarioEpic = (action$: ActionsObservable<AnyAction>) =>
   action$.pipe(
     ofType(scenarioActionTypes.GET_SCENARIO_LIST),
     exhaustMap(() =>
-      of(mockData).pipe(
-        map((res: ScenarioList) => getScenarioListSuccessAction(res)),
+      ajax.get('http://localhost:8080/v1/scenarios').pipe(
+        map((res: AjaxResponse) => getScenarioListSuccessAction(res.response)),
         catchError(err => of(getScenarioListFailedAction(err))),
       ),
     ),
@@ -27,7 +26,7 @@ export const deleteScenarioEpic = (action$: ActionsObservable<AnyAction>) =>
   action$.pipe(
     ofType(scenarioActionTypes.DELETE_SCENARIO),
     switchMap(action =>
-      of([]).pipe(
+      ajax.delete(`http://localhost:8080/v1/scenarios/${action.payload}`).pipe(
         map(() => deleteScenariosSuccessAction(action.payload)),
         catchError(err => of(deleteScenariosFailedActionAction(err))),
       ),
