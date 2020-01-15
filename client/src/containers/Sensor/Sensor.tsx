e1fe1238a0950a00e96fe8ea8828f4a7575fa873
aChloeShen <chloe.shen@access-company.com>
2020-01-15 12:37:11 +0800

    [W5D1] create sensor container

--- client/src/containers/Sensor/Sensor.tsx
+++ client/src/containers/Sensor/Sensor.tsx
@@ -1,8 +1,9 @@
 import React, { useCallback } from 'react'
 import SensorTable from './components/SensorTable'
-import { getSensorList } from '../../reducer/sensor/sensorAction'
+import { getSensorList, createSensor } from '../../reducer/sensor/sensorAction'
 import { StoreState } from '../../reducer/rootReducer'
 import { useDispatch, useSelector } from 'react-redux'
+import { SensorCreateParams } from '../../model/sensor'
 
 export default function Sensor() {
   const dispatch = useDispatch()
@@ -10,13 +11,20 @@ export default function Sensor() {
     sensorList: state.sensor.sensorList,
   }))
 
+  const onCreateSensor = useCallback(
+    (sensorBase: SensorCreateParams) => {
+      dispatch(createSensor(sensorBase))
+    },
+    [dispatch],
+  )
+
   const onGetSensorList = useCallback(() => {
     dispatch(getSensorList())
   }, [dispatch])
 
   return (
     <>
-      <SensorTable onGetSensorList={onGetSensorList} sensorList={sensorList} />
+      <SensorTable onCreateSensor={onCreateSensor} onGetSensorList={onGetSensorList} sensorList={sensorList} />
     </>
   )
 }
