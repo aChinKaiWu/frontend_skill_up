034252bc1a7f16d7b6a9f47f219708364f5d92d8
aChloeShen <chloe.shen@access-company.com>
2020-01-15 18:27:00 +0800

    [W5D1][ADD] create sensor form

--- client/src/containers/Sensor/components/SensorTable.tsx
+++ client/src/containers/Sensor/components/SensorTable.tsx
@@ -1,22 +1,48 @@
-import React, { useEffect } from 'react'
-import { Sensor } from '../../../model/sensor'
-import { Table, TableBody, TableCell, TableHead, TableRow } from '@material-ui/core'
+import React, { useEffect, useState, useCallback } from 'react'
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  TextField,
+} from '@material-ui/core'
+import { Sensor, SensorCreateParams } from '../../../model/sensor'
 
 interface Props {
+  onCreateSensor: (sensorBase: SensorCreateParams) => void
   onGetSensorList: () => void
   sensorList: Sensor[]
 }
 
 export default function SensorTable(props: Props) {
-  const { onGetSensorList, sensorList } = props
+  const { onCreateSensor, onGetSensorList, sensorList } = props
+  const [sensorBase, setSensorBase] = useState<SensorCreateParams>({
+    display_name: '',
+    type: 'A',
+    extra: '',
+  })
+  const [showForm, toggleForm] = useState(false)
   const tableHead: Array<keyof Sensor> = ['id', 'display_name', 'extra', 'type']
 
   useEffect(() => {
     onGetSensorList()
   }, [onGetSensorList])
 
+  const onSubmit = useCallback(() => {
+    onCreateSensor(sensorBase)
+    toggleForm(false)
+  }, [onCreateSensor, sensorBase])
+
   return (
     <>
+      <div>
+        <Button onClick={() => toggleForm(true)}>新增</Button>
+      </div>
       <Table>
         <TableHead>
           <TableRow>
@@ -35,6 +61,31 @@ export default function SensorTable(props: Props) {
           ))}
         </TableBody>
       </Table>
+      {showForm && (
+        <Dialog open={true}>
+          <DialogContent>
+            <div>
+              <TextField
+                label="Display name"
+                onChange={e => setSensorBase({ ...sensorBase, display_name: e.target.value })}
+              />
+            </div>
+            <div>
+              <TextField
+                label="Type"
+                onChange={e => setSensorBase({ ...sensorBase, type: e.target.value as SensorCreateParams['type'] })}
+              />
+            </div>
+            <div>
+              <TextField label="Extra" onChange={e => setSensorBase({ ...sensorBase, extra: e.target.value })} />
+            </div>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={onSubmit}>Submit</Button>
+            <Button onClick={() => toggleForm(false)}>Cancel</Button>
+          </DialogActions>
+        </Dialog>
+      )}
     </>
   )
 }
