6d635b6a527ad57be874b5afbe2eecb93fe6ed15
aChloeShen <chloe.shen@access-company.com>
2020-01-15 19:43:57 +0800

    [W5D1][FIX] create sensor form

--- client/src/containers/Sensor/components/SensorTable.tsx
+++ client/src/containers/Sensor/components/SensorTable.tsx
@@ -1,18 +1,8 @@
 import React, { useEffect, useState, useCallback } from 'react'
 import { apiResponse } from '../../../epics/utils'
-import {
-  Button,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableRow,
-  TextField,
-} from '@material-ui/core'
+import { Button, Table, TableBody, TableCell, TableHead, TableRow } from '@material-ui/core'
 import { Sensor, SensorCreateParams } from '../../../model/sensor'
+import SensorCreation from './SensorCreation'
 
 interface Props {
   onCreateSensor: (sensorBase: SensorCreateParams) => void
@@ -22,12 +12,6 @@ interface Props {
 
 export default function SensorTable(props: Props) {
   const { onCreateSensor, onGetSensorList, sensorList } = props
-  const [isLoading, setIsLoading] = useState(false)
-  const [sensorBase, setSensorBase] = useState<SensorCreateParams>({
-    display_name: '',
-    type: 'A',
-    extra: '',
-  })
   const [showForm, toggleForm] = useState(false)
   const tableHead: Array<keyof Sensor> = ['id', 'display_name', 'extra', 'type']
 
@@ -38,7 +22,6 @@ export default function SensorTable(props: Props) {
   useEffect(() => {
     const subscription = apiResponse.subscribe({
       next: () => {
-        setIsLoading(false)
         toggleForm(false)
       },
     })
@@ -46,16 +29,19 @@ export default function SensorTable(props: Props) {
     return () => subscription.unsubscribe()
   }, [])
 
-  const onSubmit = useCallback(() => {
-    onCreateSensor(sensorBase)
-  }, [onCreateSensor, sensorBase])
+  const onSubmit = useCallback(
+    (sensorBase: SensorCreateParams) => {
+      onCreateSensor(sensorBase)
+    },
+    [onCreateSensor],
+  )
 
   return (
     <>
       <div>
         <Button onClick={() => toggleForm(true)}>新增</Button>
       </div>
-      <Table style={{ background: '#fff' }}>
+      <Table>
         <TableHead>
           <TableRow>
             {tableHead.map(head => (
@@ -73,44 +59,7 @@ export default function SensorTable(props: Props) {
           ))}
         </TableBody>
       </Table>
-      {showForm && (
-        <Dialog open={true}>
-          <DialogContent>
-            <div>
-              <TextField
-                disabled={isLoading}
-                label="Display name"
-                onChange={e => setSensorBase({ ...sensorBase, display_name: e.target.value })}
-              />
-            </div>
-            <div>
-              <TextField
-                disabled={isLoading}
-                label="Type"
-                onChange={e => setSensorBase({ ...sensorBase, type: e.target.value as SensorCreateParams['type'] })}
-              />
-            </div>
-            <div>
-              <TextField
-                disabled={isLoading}
-                label="Extra"
-                onChange={e => setSensorBase({ ...sensorBase, extra: e.target.value })}
-              />
-            </div>
-          </DialogContent>
-          <DialogActions>
-            <Button
-              onClick={() => {
-                setIsLoading(true)
-                onSubmit()
-              }}
-            >
-              Submit
-            </Button>
-            <Button onClick={() => toggleForm(false)}>Cancel</Button>
-          </DialogActions>
-        </Dialog>
-      )}
+      {showForm && <SensorCreation onCancel={() => toggleForm(false)} onSubmit={onSubmit} />}
     </>
   )
 }
